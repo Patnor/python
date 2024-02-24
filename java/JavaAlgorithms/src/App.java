73692f4b6c83331d5b2dcd3fc7bb27d4820d1481
Patrick Normile <43999718+Patnor@users.noreply.github.com>
2024-02-23 17:40:07 -0800

    implemented merge sort in java

--- java/JavaAlgorithms/src/App.java
+++ java/JavaAlgorithms/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -5,8 +6,17 @@ public class App {
         MergeSortPJN mergeSortPJN = new MergeSortPJN();
         mergeSortPJN.sayGoodbye();
         // Rest of your code...
+        int [] myArray = {1,6,2,5,4};
+        System.out.println(Arrays.toString(MergeSortPJN.merge_sort(myArray)));
 
-        
+        int[] array4 = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+        System.out.println(Arrays.toString(MergeSortPJN.merge_sort(array4)));
 
+        // Test case 5: Array with negative numbers
+        int[] array5 = {-5, -2, 6, -9, -1, -3};
+        System.out.println(Arrays.toString(MergeSortPJN.merge_sort(array5)));
+
+
+       
     }
 }
--- java/JavaAlgorithms/src/MergeSortPJN.java
+++ java/JavaAlgorithms/src/MergeSortPJN.java
@@ -1,4 +1,51 @@
+import java.util.Arrays;
+
 public class MergeSortPJN {
+
+    public static int[] merge_sort(int[] array) {
+
+        if (array.length <= 1) {
+            return array;
+        }
+
+        int mid = array.length / 2;
+
+        int[] a_left = merge_sort(Arrays.copyOfRange(array, 0, mid));
+        int[] a_right = merge_sort(Arrays.copyOfRange(array, mid, array.length));
+
+        return merge(a_left, a_right);
+
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+
+        int[] result = new int[left.length + right.length];
+        int i = 0;
+        int j = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                result[i + j] = left[i];
+                i++;
+            } else {
+                result[i + j] = right[j];
+                j++;
+            }
+        }
+
+        while (i < left.length) {
+            result[i + j] = left[i];
+            i++;
+        }
+
+        while (j < right.length) {
+            result[i + j] = right[j];
+            j++;
+        }
+
+        return result;
+    }
+
     public void sayGoodbye() {
         System.out.println("Goodbye!");
     }
