1e86ac7d3d8435c20bc2002fa88659f5478000d9
Patrick Normile <43999718+Patnor@users.noreply.github.com>
2024-03-01 17:39:37 -0800

    Insertion sort for java implemented.

--- java/JavaAlgorithms/src/App.java
+++ java/JavaAlgorithms/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
    
-        // Rest of your code...
+        // merge sort
         int [] myArray = {1,6,2,5,4};
         System.out.println(Arrays.toString(MergeSortPJN.merge_sort(myArray)));
 
@@ -16,6 +16,13 @@ public class App {
         System.out.println(Arrays.toString(MergeSortPJN.merge_sort(array5)));
 
 
+        // insertion sort
+        int [] myArray2 = {1,6,2,5,4, 3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+        int[] arrayInsert5 = {-5, -2, 6, -9, -1, -3};
+        InsertionSortPJN.sort(myArray2);
+        InsertionSortPJN.sort(arrayInsert5);
+        System.out.println(Arrays.toString(myArray2));
+        System.out.println(Arrays.toString(arrayInsert5));
        
     }
 }
---
+++ java/JavaAlgorithms/src/InsertionSortPJN.java
@@ -0,0 +1,28 @@
+/**
+ * The InsertionSortPJN class provides a static method to sort an array 
+ * using the Insertion Sort algorithm.
+ * 
+ * The time complexity of the insertion sort algorithm is O(n^2) in the worst case, 
+ * where n is the number of elements in the array. However, it performs well for 
+ * small arrays or partially sorted arrays.
+ */
+public class InsertionSortPJN {
+    /**
+     * Sorts the given array in ascending order using the Insertion Sort algorithm.
+     * 
+     * @param arr the array to be sorted
+     */
+    public static void sort(int [] arr){
+        for(int i = 1; i < arr.length; i++){
+            int tmp = arr[i];
+            int j = i - 1;
+             // Enter a while loop if 'j' is greater than 0 and the value 
+             // of 'tmp' is less than the value at index 'j' of the array 'arr'             
+            while(j > 0 && tmp < arr[j]){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = tmp;
+        }
+    }
+}
