92d4bce5975dd905ad278fa7899c0b4cbf56b3b4
Patrick Normile <43999718+Patnor@users.noreply.github.com>
2024-03-20 19:42:15 -0700

    removed the c# project

--- c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab11c227decd541d1616aab327636d1d88469418")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc2e0e5cbeef6f02242abd83d00f0998d00719f")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
